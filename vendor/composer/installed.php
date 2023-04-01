c725be677bc6d1579f9036616263c33394a35a13
rqb <rqb@qq.com>
2023-04-02 07:46:09 +0800

    代码

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'raoqingbing/demo',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'e79ca59857e10e70d586a666b3949b62c2b3b6f5',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'raoqingbing/demo' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'e79ca59857e10e70d586a666b3949b62c2b3b6f5',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
